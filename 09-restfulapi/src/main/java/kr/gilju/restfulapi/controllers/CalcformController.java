93e72c86169e6bf599099a8cfb6da6a859f35233
Gilju Song <rlfwn@gmail.com>
2024-10-30 16:37:40 +0900

    ajax html화면으로 계산기 출력하기

---
+++ 09-restfulapi/src/main/java/kr/gilju/restfulapi/controllers/CalcformController.java
@@ -0,0 +1,26 @@
+package kr.gilju.restfulapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.gilju.restfulapi.helpers.RestHelper;
+
+@Controller
+public class CalcformController {
+  
+
+    @Autowired
+  private RestHelper restHelper;
+  /**
+ * 발송폼을 출력하는 메소드
+ * 웹 브라우저에게 HTML을 전송한다
+ * 프론트엔드 역할
+ * @return 
+ */
+  @GetMapping("/calcform")
+  public String calcForm() {
+    return "calcform.html";
+  }
+
+}
--- 09-restfulapi/src/main/java/kr/gilju/restfulapi/controllers/RestCalcController.java
+++ 09-restfulapi/src/main/java/kr/gilju/restfulapi/controllers/RestCalcController.java
@@ -1,97 +1,176 @@
 package kr.gilju.restfulapi.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+import kr.gilju.restfulapi.helpers.RestHelper;
 
 import java.util.LinkedHashMap;
-
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletResponse;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
+/**
+ * RESTful API 방식의 컨트롤러
+ * x 와 y의 사칙 연산을 수행한다
+ * 단, x와 y는 모두 0보다 커야 한다
+ */
 @RestController
 public class RestCalcController {
 
+  @Autowired
+  private RestHelper restHelper;
 
+  // GET //
   @GetMapping("/my_calc")
-  public Map<String, Object> getcalc(
+  public Map<String, Object> plus(
       HttpServletResponse response,
-      @RequestParam("x") int x,
-      @RequestParam("y") int y) {
+      @RequestParam(value="x", defaultValue="0") int x,
+      @RequestParam(value="y", defaultValue="0") int y) {
 
-    // 3) 제이슨으로 변환될 map객체 구성
-    Map<String, Object> result = new LinkedHashMap<String, Object>();
-
-    result.put("x", x);
-    result.put("y", y);
-    result.put("result", x + y);  // 더하기
-
-    return result;
-  }
-
-
-
-  @PostMapping("/my_calc")
-  public Map<String, Object> postcalc(
-      HttpServletResponse response,
-      @RequestParam("x") int x,
-      @RequestParam("y") int y) {
-
-    // 3) 제이슨으로 변환될 map객체 구성
-    Map<String, Object> result = new LinkedHashMap<String, Object>();
-
-    result.put("x", x);
-    result.put("y", y);
-    result.put("result", x + y); // 더하기
-
-    return result;
-  }
-
-  @PutMapping("/my_calc")
-  public Map<String, Object> putcalc(
-      HttpServletResponse response,
-      @RequestParam("x") int x,
-      @RequestParam("y") int y) {
-
-    // 3) 제이슨으로 변환될 map객체 구성
-    Map<String, Object> result = new LinkedHashMap<String, Object>();
-
-    result.put("x", x);
-    result.put("y", y);
-    result.put("result", x * y); //곱하기
-
-    return result;
-  }
-
-  @DeleteMapping("/my_calc")
-  public Map<String, Object> Deletecalc(
-      HttpServletResponse response,
-      @RequestParam("x") int x,
-      @RequestParam("y") int y) {
-
-
-    // 3) 제이슨으로 변환될 map객체 구성
-    Map<String, Object> result = new LinkedHashMap<String, Object>();
-
-    if (x == 0 || y == 0) {
-      result.put("result", "0으로 나눌 수 없습니다");
-    } else {
-      result.put("result", x/y);
+    /** 1) 파라미터 유효성 검사 */
+    if (x < 0) {
+      // case 1) 에러내용을 직접 문자열로 전달
+      return restHelper.badRequest("x는 0보다 커야 합니다");
     }
 
-    
+    if (y < 0) {
+      // case 2) 에러내용을 예외 객체로 전달하는 경우
+      // catch 블록에서 사용하는 경우를 가정
+      NumberFormatException ex = new NumberFormatException("y는 0보다 커야 합니다");
+      return restHelper.badRequest(ex);
+    }
 
-    return result;
+    /** 2) 처리해야할 로직 수행 (DB연동 등을 가정) */
+    int result = 0;
+
+    try {
+      result = x + y;
+    } catch (Exception e) {
+      return restHelper.serverError(e);
+    }
+
+    /** 3) 응답 결과를 구성 */
+    Map<String, Object> output = new LinkedHashMap<String, Object>();
+
+    output.put("x", x);
+    output.put("y", y);
+    output.put("result", result);
+
+    return output;
+  }
+
+  // POST //
+  @PostMapping("/my_calc")
+  public Map<String, Object> minus(@RequestParam("x") int x, @RequestParam("y") int y) {
+    /** 1) 파라미터 유효성 검사 */
+    if (x < 0) {
+      // case 1) 에러내용을 직접 문자열로 전달
+      return restHelper.badRequest("x는 0보다 커야 합니다");
+    }
+
+    if (y < 0) {
+      // case 2) 에러내용을 예외 객체로 전달하는 경우
+      // catch 블록에서 사용하는 경우를 가정
+      NumberFormatException ex = new NumberFormatException("y는 0보다 커야 합니다");
+      return restHelper.badRequest(ex);
+    }
+
+    /** 2) 처리해야할 로직 수행 (DB연동 등을 가정) */
+    int result = 0;
+
+    try {
+      result = x - y;
+    } catch (Exception e) {
+      return restHelper.serverError(e);
+    }
+
+    /** 3) 응답 결과를 구성 */
+    Map<String, Object> output = new LinkedHashMap<String, Object>();
+
+    output.put("x", x);
+    output.put("y", y);
+    output.put("result", result);
+
+    return output;
+
+  }
+
+  // PUT //
+  @PutMapping("/my_calc")
+  public Map<String, Object> times(@RequestParam("x") int x, @RequestParam("y") int y) {
+    /** 1) 파라미터 유효성 검사 */
+    if (x < 0) {
+      // case 1) 에러내용을 직접 문자열로 전달
+      return restHelper.badRequest("x는 0보다 커야 합니다");
+    }
+
+    if (y < 0) {
+      // case 2) 에러내용을 예외 객체로 전달하는 경우
+      // catch 블록에서 사용하는 경우를 가정
+      NumberFormatException ex = new NumberFormatException("y는 0보다 커야 합니다");
+      return restHelper.badRequest(ex);
+    }
+
+    /** 2) 처리해야할 로직 수행 (DB연동 등을 가정) */
+    int result = 0;
+
+    try {
+      result = x * y;
+    } catch (Exception e) {
+      return restHelper.serverError(e);
+    }
+
+    /** 3) 응답 결과를 구성 */
+    Map<String, Object> output = new LinkedHashMap<String, Object>();
+
+    output.put("x", x);
+    output.put("y", y);
+    output.put("result", result);
+
+    return output;
+  }
+
+
+  // DELET //
+  @DeleteMapping("/my_calc")
+  public Map<String, Object> divied(@RequestParam("x") int x, @RequestParam("y") int y) {
+    /** 1) 파라미터 유효성 검사 */
+    if (x < 0) {
+      // case 1) 에러내용을 직접 문자열로 전달
+      return restHelper.badRequest("x는 0보다 커야 합니다");
+    }
+
+    if (y < 0) {
+      // case 2) 에러내용을 예외 객체로 전달하는 경우
+      // catch 블록에서 사용하는 경우를 가정
+      NumberFormatException ex = new NumberFormatException("y는 0보다 커야 합니다");
+      return restHelper.badRequest(ex);
+    }
+
+    /** 2) 처리해야할 로직 수행 (DB연동 등을 가정) */
+    int result = 0;
+
+    try {
+      result = x / y;
+    } catch (Exception e) {
+      return restHelper.serverError(e);
+    }
+
+    /** 3) 응답 결과를 구성 */
+    Map<String, Object> output = new LinkedHashMap<String, Object>();
+
+    output.put("x", x);
+    output.put("y", y);
+    output.put("result", result);
+
+    return output;
+ 
   }
 }
-
-
-
